5ddc27a584fd04f7cddc3319a7dc881367171513
xiaoweimei <1227380089@qq.com>
2019-07-28 17:26:31 +0800

    第一次build

---
+++ build/precache-manifest.37f9a21d7a256155a543a460e12f1b9c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e25507e73e3ae9efba323968b5fbfa8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "af1997ee70ba8a112ddc",
+    "url": "/static/css/main.b6cdaab1.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "af1997ee70ba8a112ddc",
+    "url": "/static/js/main.174d24e1.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
